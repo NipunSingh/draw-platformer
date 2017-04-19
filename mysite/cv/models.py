e4a35725df6bcb0009b767991cdf27282f6c3a41
Nipun Singh <nipunsingh@d-172-25-249-80.dhcp.virginia.edu>
2017-04-19 13:51:29 -0400

    Got rid of boilerplate models

--- mysite/cv/models.py
+++ mysite/cv/models.py
@@ -4,19 +4,6 @@ from django.db import models
 
 # Create your models here.
 
-from django.db import models
-
-
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-
 class GameMap(models.Model):
     title = models.CharField(max_length=200)
     map = models.TextField()
