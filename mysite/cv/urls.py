20ddcbf198243a6d8895f437acbb1d70c0f92546
NipunSingh <ns2se@virginia.edu>
2017-05-28 12:30:47 -0400

    Better url names

--- mysite/cv/urls.py
+++ mysite/cv/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'game/(?P<name>\w{0,50})/$', views.game, name='game'),
-    url(r'game/(?P<name>\w{0,50})/updatescore/', views.update_score, name='game'),
+    url(r'game/(?P<name>\w{0,50})/$', views.game, name='play_game'),
+    url(r'game/(?P<name>\w{0,50})/updatescore/', views.update_score, name='update_score'),
     url(r'^', views.upload_file, name='upload'),
 ]
 
